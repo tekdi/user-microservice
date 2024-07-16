57348579dd4d4034c60efc7fde9cac13ea7e1b45
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2024-07-16 18:09:09 +0530

    PS-1243 Master data management

--- src/adapters/fieldsservicelocator.ts
+++ src/adapters/fieldsservicelocator.ts
@@ -3,9 +3,12 @@ import { FieldsDto } from "src/fields/dto/fields.dto";
 import { FieldValuesDto } from "src/fields/dto/field-values.dto";
 import { FieldValuesSearchDto } from "src/fields/dto/field-values-search.dto";
 import { Response } from "express";
+import { FieldsUpdateDto } from "src/fields/dto/fields-update.dto";
 export interface IServicelocatorfields {
   //fields
   createFields(request: any, fieldsDto: FieldsDto, response: Response);
+  //Update
+  updateFields(fieldId: any, request: any, fieldsUpdateDto: FieldsUpdateDto, response: Response);
   // getFields(tenantId, fieldsId, request);
   searchFields(tenantid, request: any, fieldsSearchDto: FieldsSearchDto, response: Response);
   // updateFields(fieldsId: string, request: any, fieldsDto: FieldsDto);
@@ -15,6 +18,6 @@ export interface IServicelocatorfields {
   searchFieldValues(request: any, fieldValuesSearchDto: FieldValuesSearchDto, response: Response);
   updateFieldValues(id: string, request: any, fieldValuesDto: FieldValuesDto);
   getFieldOptions(request: any, fieldName: string, controllingfieldfk: string, context: string, contextType: string, response: Response);
-  getFormCustomField(requiredData ,response);
+  getFormCustomField(requiredData, response);
 
 }
