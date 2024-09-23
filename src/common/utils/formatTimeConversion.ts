88949752cdd2ad3a11d9c3a27fc1a8c66de8ebed
poojakarma <pooja.karma@tekditechnologies.com>
2024-09-23 17:17:12 +0530

    added changes for reset api and integrate email after reset successfully

--- src/common/utils/formatTimeConversion.ts
+++ src/common/utils/formatTimeConversion.ts
@@ -1,12 +1,12 @@
 export function formatTime(milliseconds) {
     const seconds = Math.floor(milliseconds / 1000);
     if (seconds < 60) {
-        return `${seconds} seconds`;
+        return `${seconds} second${seconds === 1 ? '' : 's'}`;
     } else if (seconds < 3600) { // less than 1 hour
         const minutes = Math.floor(seconds / 60);
-        return `${minutes} minutes`;
+        return `${minutes} minute${minutes === 1 ? '' : 's'}`;
     } else {
         const hours = Math.floor(seconds / 3600);
-        return `${hours} hours`;
+        return `${hours} hour${hours === 1 ? '' : 's'}`;
     }
 }
