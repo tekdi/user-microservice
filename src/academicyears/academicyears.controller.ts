dda981c7cb34f51cc51bd8bf3babb4b93fd9dd4e
poojakarma <pooja.karma@tekditechnologies.com>
2024-10-07 12:23:44 +0530

    added search API

--- src/academicyears/academicyears.controller.ts
+++ src/academicyears/academicyears.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Post, Res, Headers, UsePipes, ValidationPipe, UseFilters, BadRequestException } from '@nestjs/common';
-import { ApiBody, ApiCreatedResponse, ApiHeader, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, Res, Headers, UsePipes, ValidationPipe, UseFilters, BadRequestException, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { ApiBody, ApiCreatedResponse, ApiHeader, ApiInternalServerErrorResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from "express";
 import { AcademicYearDto } from './dto/academicyears-create.dto';
 import { AcademicYearAdapter } from './academicyearsadaptor';
@@ -8,6 +8,7 @@ import { APIID } from '@utils/api-id.config';
 import { API_RESPONSES } from '@utils/response.messages';
 import { DateValidationPipe } from 'src/common/pipes/date-validation.pipe';
 import { isUUID } from 'class-validator';
+import { AcademicYearSearchDto } from './dto/academicyears-search.dto';
 
 
 @ApiTags("Academicyears")
@@ -30,6 +31,35 @@ export class AcademicyearsController {
             throw new BadRequestException(API_RESPONSES.TENANTID_VALIDATION);
         }
         let result = await this.academicYearAdapter.buildAcademicYears().createAcademicYear(academicyearsService, tenantId, response)
+        return response.status(result.statusCode).json(result);
+    }
+
+    @UseFilters(new AllExceptionsFilter(APIID.ACADEMICYEAR_LIST))
+    @Post('/list')
+    @UsePipes(new ValidationPipe({ transform: true }))
+    @ApiHeader({ name: "tenantid" })
+    @ApiBody({ type: AcademicYearSearchDto })
+    @ApiCreatedResponse({ description: API_RESPONSES.ACADEMICYEAR })
+    async getAcademicYearList(@Body() academicYearSearchDto: AcademicYearSearchDto, @Res() response: Response, @Headers() headers) {
+        const tenantId = headers["tenantid"];
+        if (!tenantId || !isUUID(tenantId)) {
+            throw new BadRequestException(API_RESPONSES.TENANTID_VALIDATION);
+        }
+        let result = await this.academicYearAdapter.buildAcademicYears().getAcademicYearList(academicYearSearchDto, tenantId, response)
+        return response.status(result.statusCode).json(result);
+    }
+
+    @UseFilters(new AllExceptionsFilter(APIID.ACADEMICYEAR_GET))
+    @Get('/:id')
+    @ApiResponse({ status: 200, description: API_RESPONSES.ACADEMICYEAR_GET_SUCCESS })
+    @ApiInternalServerErrorResponse({
+        description: API_RESPONSES.INTERNAL_SERVER_ERROR,
+    })
+    async getAcademicYearById(
+        @Param('id', new ParseUUIDPipe()) id: string,
+        @Res() response: Response,
+    ) {
+        let result = await this.academicYearAdapter.buildAcademicYears().getAcademicYearById(id, response)
         return response.status(result.statusCode).json(result);
     }
 }
--- src/adapters/academicyearsservicelocater.ts
+++ src/adapters/academicyearsservicelocater.ts
@@ -1,6 +1,9 @@
 import { AcademicYearDto } from "src/academicyears/dto/academicyears-create.dto";
 import { Response } from "express";
+import { AcademicYearSearchDto } from "src/academicyears/dto/academicyears-search.dto";
 
 export interface IServicelocatorAcademicyear {
     createAcademicYear(academicYearDto: AcademicYearDto, tenantId, response: Response): Promise<any>;
+    getAcademicYearList(academicYearSearchDto: AcademicYearSearchDto, tenantId, response: Response)
+    getAcademicYearById(id, response: Response)
 }
