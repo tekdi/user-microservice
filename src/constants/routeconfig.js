4d9ff7c7c7511ef57593b3a0be6d373b1a38f2da
Arati Tekdi <arati.kulkarni@tekditechnologies.com>
2025-01-14 12:02:05 +0530

    Formated config json

--- src/constants/routeconfig.js
+++ src/constants/routeconfig.js
@@ -1,30 +1,31 @@
 {
-	routes: [
-		{
-			sourceRoute: '/interface/v1/account/create',
-			type: 'POST',
-			priority: 'MUST_HAVE',
-			inSequence: false,
-			orchestrated: false,
-			targetPackages: [
-				{
-					basePackageName: 'user',
-					packageName: 'shiksha-user',
-				},
-			],
-		},
-		{
-			sourceRoute: '/interface/v1/account/login',
-			type: 'POST',
-			priority: 'MUST_HAVE',
-			inSequence: false,
-			orchestrated: false,
-			targetPackages: [
-				{
-					basePackageName: 'user',
-					packageName: 'shiksha-user',
-				},
-			],
-		},
-	],
-}
+	"routes": [
+	  {
+		"sourceRoute": "/interface/v1/account/create",
+		"type": "POST",
+		"priority": "MUST_HAVE",
+		"inSequence": false,
+		"orchestrated": false,
+		"targetPackages": [
+		  {
+			"basePackageName": "user",
+			"packageName": "shiksha-user"
+		  }
+		]
+	  },
+	  {
+		"sourceRoute": "/interface/v1/account/login",
+		"type": "POST",
+		"priority": "MUST_HAVE",
+		"inSequence": false,
+		"orchestrated": false,
+		"targetPackages": [
+		  {
+			"basePackageName": "user",
+			"packageName": "shiksha-user"
+		  }
+		]
+	  }
+	]
+  }
+  
