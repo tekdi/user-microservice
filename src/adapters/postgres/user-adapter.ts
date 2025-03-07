b5fce3aa9c0a2ba422394136ee7b86cf635f3b93
Shubham <shubham.jha@tekditechnologies.com>
2025-03-07 17:30:46 +0530

    Removed Console

--- src/adapters/postgres/user-adapter.ts
+++ src/adapters/postgres/user-adapter.ts
@@ -476,7 +476,6 @@ export class PostgresUserService implements IServicelocator {
               index++;
               break;
           }
-        console.log(whereCondition,"Shubham");
         } else {
           //For custom field store the data in key value pear
           searchCustomFields[key] = value;
@@ -549,7 +548,6 @@ export class PostgresUserService implements IServicelocator {
     }
 
     //Get user core fields data
-    console.log(whereCondition,"Sanket")
     const query = `SELECT U."userId", U."username",U."email", U."firstName",UTM."tenantId", U."middleName", U."lastName", U."gender", U."dob", R."name" AS role, U."mobile", U."createdBy",U."updatedBy", U."createdAt", U."updatedAt", U."status", COUNT(*) OVER() AS total_count 
       FROM  public."Users" U
       LEFT JOIN public."CohortMembers" CM 
