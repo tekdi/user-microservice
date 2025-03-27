1668e1b47fc2d00da6579a5a4ae5a2c5949ffc8e
Tushar <tushar.mahajan@tekditechnologies.com>
2025-03-27 11:19:03 +0530

    Issue #000000 Change Mobile validation now its 7 to 9 digit

--- src/adapters/postgres/user-adapter.ts
+++ src/adapters/postgres/user-adapter.ts
@@ -1273,7 +1273,7 @@ export class PostgresUserService implements IServicelocator {
           userCreateDto.mobile
         );
         if (!checkValidMobile) {
-          errorCollector.addError(`Mobile number must be 9 digits long`);
+          errorCollector.addError(`Mobile number must be 7 to 9 digits long`);
         }
       }
 
