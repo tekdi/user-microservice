a909b4571ba684681646b8f776676e27d27ee8e5
vaibhav_s <vaibhav_s@tekditechnologies.com>
2025-06-09 16:26:43 +0530

    Issues #241902 Chore: Coderabbit changes

--- src/cohortMembers/dto/cohortMembers.dto.ts
+++ src/cohortMembers/dto/cohortMembers.dto.ts
@@ -1,7 +1,24 @@
-import { Expose } from "class-transformer";
+import { Expose, Type } from "class-transformer";
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsUUID, IsOptional, IsString, IsArray } from "class-validator";
+import { IsNotEmpty, IsUUID, IsOptional, IsString, IsArray, IsEnum, ValidateNested } from "class-validator";
 import { MemberStatus } from "../entities/cohort-member.entity";
+
+export class CustomFieldDto {
+  @ApiProperty({
+    type: String,
+    description: "The unique identifier of the field",
+  })
+  @IsUUID(undefined, { message: "Field ID must be a valid UUID" })
+  @IsNotEmpty()
+  fieldId: string;
+
+  @ApiProperty({
+    description: "The value of the custom field",
+    example: "Some value or array of values",
+  })
+  @IsNotEmpty()
+  value: string | string[];
+}
 
 export class CohortMembersDto {
   //generated fields
@@ -57,6 +74,9 @@ export class CohortMembersDto {
   })
   @Expose()
   @IsOptional()
+  @IsEnum(MemberStatus, {
+    message: "Status must be one of the following values: " + Object.values(MemberStatus).join(", ")
+  })
   status: MemberStatus;
 
   @ApiProperty({
@@ -70,14 +90,16 @@ export class CohortMembersDto {
   statusReason?: string;
 
   @ApiProperty({
-    type: Array,
+    type: [CustomFieldDto],
     description: "Custom fields for the cohort member",
     required: false,
   })
   @Expose()
   @IsOptional()
   @IsArray()
-  customFields?: any[];
+  @ValidateNested({ each: true })
+  @Type(() => CustomFieldDto)
+  customFields?: CustomFieldDto[];
 
   constructor(obj: any) {
     Object.assign(this, obj);
