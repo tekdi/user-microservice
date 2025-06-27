2a441af3aa9cc12a6641f2906dcafe9422bfba95
Tushar <tushar.mahajan@tekditechnologies.com>
2025-06-27 11:29:15 +0530

    Issue #000000 Feat: reCAPCHA Verification

--- src/user/recaptcha.service.ts
+++ src/user/recaptcha.service.ts
@@ -1,24 +1,3 @@
-// import { Injectable, UnauthorizedException } from '@nestjs/common';
-// import axios from 'axios';
-
-// @Injectable()
-// export class RecaptchaService {
-//   async validateToken(token: string): Promise<void> {
-//     const secret = process.env.RECAPTCHA_SECRET_KEY;
-//     const recaptchaSite = process.env.RECAPTCHA_SITE;
-//     const response = await axios.post(recaptchaSite, null, {
-//       params: {
-//         secret,
-//         response: token,
-//       },
-//     });
-
-//     if (!response.data.success) {
-//       throw new UnauthorizedException('CAPTCHA validation failed');
-//     }
-//   }
-// }
-
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
