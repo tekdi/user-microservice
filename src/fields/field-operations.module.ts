d36127713e0b3663dd86017d35ed7f12e52a90ca
Tushar <tushar.mahajan@tekditechnologies.com>
2025-07-16 19:57:49 +0530

    Issue #000000 Fix: Field Value boolean type issue

--- src/fields/field-operations.module.ts
+++ src/fields/field-operations.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Fields } from './entities/fields.entity';
 import { FieldValues } from './entities/fields-values.entity';
 import { FieldsService } from './fields.service';
+import { FormsModule } from '../forms/forms.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Fields]),
-    TypeOrmModule.forFeature([FieldValues])
+    TypeOrmModule.forFeature([FieldValues]),
+    FormsModule // Import FormsModule to get access to FormsService
   ],
   providers: [
     FieldsService,
