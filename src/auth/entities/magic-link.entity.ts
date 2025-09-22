b61fb091d894670da1299f2e8d06575b80a0da82
apurvaubade <apurva.bade@tekditechnologies.com>
2025-09-22 11:27:16 +0530

    change userId type to uuid

--- src/auth/entities/magic-link.entity.ts
+++ src/auth/entities/magic-link.entity.ts
@@ -11,7 +11,7 @@ export class MagicLink {
   @Column({ type: 'varchar', length: 255 })
   identifier: string;
 
-  @Column({ type: 'varchar', length: 255, nullable: true })
+  @Column({ type: 'uuid'})
   userId: string;
 
   @Column({ type: 'varchar', length: 20 })
