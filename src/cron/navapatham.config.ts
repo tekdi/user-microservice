0d7f22443730a2bb858608a97cbcdab714d2b2a5
Shubham <shubham.jha@tekditechnologies.com>
2026-01-13 12:42:51 +0530

    Qa Config Updated

--- src/cron/navapatham.config.ts
+++ src/cron/navapatham.config.ts
@@ -2,5 +2,5 @@
 // Add your IDs directly here
 export const navapathamConfig = {
   tenantId: 'ef99949b-7f3a-4a5f-806a-e67e683e38f3',
-  academicYearId: '9a9e0daa-50dd-4d0e-8d10-36e7bc808f88',
+  academicYearId: '851687bb-422e-4a22-b27f-6b66fa304bec',
 };
