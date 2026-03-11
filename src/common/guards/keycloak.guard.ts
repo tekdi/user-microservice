1e6c362b5b745d626a6d3768cf804d75c76793bc
ttpl-rt-252 <kamalakanta_b@tekditechnologies.com>
2026-03-11 14:42:20 +0530

    Task #255125 chore: Modify certificate API to Generte the certificate for the none event lessons

--- src/common/guards/keycloak.guard.ts
+++ src/common/guards/keycloak.guard.ts
@@ -1,22 +1,5 @@
-import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard("jwt-keycloak") implements CanActivate {
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const serviceTokenHeader = request.headers['x-service-token'];
-    
-    // Check if internal service token bypasses auth
-    if (
-      serviceTokenHeader && 
-      process.env.SERVICE_AUTH_TOKEN && 
-      serviceTokenHeader === process.env.SERVICE_AUTH_TOKEN
-    ) {
-      return true;
-    }
-
-    // Otherwise, fallback to the standard JWT Keycloak auth
-    return (super.canActivate(context) as Promise<boolean>);
-  }
-}
+export class JwtAuthGuard extends AuthGuard("jwt-keycloak") {}
